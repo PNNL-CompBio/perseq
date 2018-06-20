9bba4c967a21c38d7e1af1491197ab27d26222c5
Joe Brown <brwnjm@gmail.com>
2018-06-20 11:57:34 -0700

    tracks reference updating

--- scripts/update_ko_list.sh
+++
@@ -1,6 +0,0 @@
-# keggftp/pathway/map.tar.gz for map.list
-# keggftp/genes/fasta
-# keggftp/genes/links/*
-
-curl http://rest.kegg.jp/list/ko > $(date +"%Y%m%d")_ko_list.txt
-curl http://rest.kegg.jp/list/pathway > $(date +"%Y%m%d")_pathway_list.txt
