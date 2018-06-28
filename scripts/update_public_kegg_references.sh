cb2ab8cc8eed66da48df349f8aa55dec7de7b678
Joe Brown <brwnjm@gmail.com>
2018-06-28 13:16:26 -0700

    adds genome list

--- scripts/update_public_kegg_references.sh
+++ scripts/update_public_kegg_references.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 outdir=${1:-"tests/refdata"}
 
-curl "http://rest.kegg.jp/list/{ko,pathway}" \
+curl "http://rest.kegg.jp/list/{genome,ko,pathway}" \
     --create-dirs --output "${outdir}/#1_list.txt"
 curl "http://www.genome.jp/kegg-bin/download_htext?htext=ko00001&format=json" \
     --location --create-dirs --output "${outdir}/hierarchy.json"
