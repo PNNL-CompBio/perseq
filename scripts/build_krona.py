b38bb627d4a800cba2abd424fab0fb1a9412e1cf
nzavoshy <nzavoshy@uoregon.edu>
2018-08-08 14:23:47 -0700

    fixed fixmes

--- scripts/build_krona.py
+++ scripts/build_krona.py
@@ -16,12 +16,11 @@ def build_tax(tax_file, output):
     # read in tax file to order level from build_tax_table rule
     test_tax = pd.read_table(tax_file)
     col_names = list(test_tax)[1:]
-    # separate the taxonomic levels into columns
-    test_tax["Kingdom"] = test_tax.taxonomy_order.str.split("; ").str.get(0)
-    test_tax["Phylum"] = test_tax.taxonomy_order.str.split("; ").str.get(1)
-    test_tax["Class"] = test_tax.taxonomy_order.str.split("; ").str.get(2)
-    test_tax["Order"] = test_tax.taxonomy_order.str.split("; ").str.get(3)
+    taxonomies = ['Kingdom','Phylum','Class','Order']
+    for i,item in enumerate(taxonomies,0):
+        test_tax[item] = test_tax.taxonomy_order.str.split("; ").str.get(i)
     # proposed cleanup for above
+    # I don't think this proposal works so I tried a for loop above
     # test_tax[["Kingdom", "Phylum", "Class", "Order"]] = test_tax.taxonomy_order.str.split("; ").str[0:4]
     # generate individual files per sample
     for item in col_names:
@@ -34,11 +33,9 @@ def build_tax(tax_file, output):
         )
 
 
-def build_ec(output, ec_file, ec_file_from_summaries, dat_file):
+def build_ec_dict(ec_file, dat_file):
     """
-    Parses enzyme commission numbers in their hierarchy. Also Parses
-    the function_ec.txt file to determine what enzyme pathways are present in the
-    samples. Builds a KronaPlot of this hirarchical data as well
+    Parses enzyme commission numbers in their hierarchy
     """
 
     with open(ec_file) as file, open(dat_file) as dat_file:
@@ -63,65 +60,61 @@ def build_ec(output, ec_file, ec_file_from_summaries, dat_file):
                 d[path].insert(0, first_level)
                 d[path].insert(1, second_level)
 
-        n = 0
-        # FIXME this should be compiled
-        regex = r"^[0-9]+[\.]"
         # need to grab the lowest level of ec numbers
         for line in dat_file:
-            # FIXME why is this in a try/except block?
-            try:
-                if line.startswith("ID"):
-                    line = line.strip().split("  ")[1].strip()
-            except:
-                pass
-                # print('problem line',line)
-            # use compiled regex
-            if re.match(regex, line):
-                # print('match',line)
-                # FIXME i would change this so we're not iterating over the thing being iterated upon in the loop
+            if line.startswith("ID"):
+                line = line.strip().split("  ")[1].strip()
                 desc = next(dat_file).split("  ")[1].strip()
                 first_levels = d[line.rpartition(".")[0] + ".-"]
                 for level in first_levels:
                     d[line].append(level)
                 d[line].append(desc)
-    # FIXME everything above this line should be its own function
-    # FIXME why not 'with' block here?
-    ec_file_sum = open(ec_file_from_summaries)
-    next(ec_file_sum)
-    new_ec_dict = {"ec": []}
-    # split ec numbers that mapped to multiple
-    for item in ec_file_sum:
-        toks = item.partition("\t")[0]
-        ec = toks.split(";")
-        # print(ec)
-        first_item = ec[0][:5]
-        if len(ec) == 1:
-            new_ec_dict["ec"].append(ec[0])
-        else:
-            n = 0
-            for item in ec[1:]:
-                if item[:5] == first_item:
-                    new_ec_dict["ec"].append(item[:5] + ".-")
-                    # print('same',item,first_item)
-                elif item[:3] == first_item[:3]:
-                    new_ec_dict["ec"].append(item[:3] + ".-.-")
-                    # print('same to high level',item,first_item)
-                elif item[:1] == first_item[:1]:
-                    new_ec_dict["ec"].append(item[:1] + ".-.-.-")
-    ec_file_sum.close()
-    # FIXME everthing above this line should be its own function
-    # build dictionary
+    return d
+
+
+def parse_ec_file(ec_file_from_summaries):
+    '''
+    Split sequences that aligned to multiple ec numbers into last common ec number
+    shared amongst matches
+    '''
+    with open(ec_file_from_summaries) as ec_file_sum:
+
+        next(ec_file_sum)
+        new_ec_dict = {"ec": []}
+        # split ec numbers that mapped to multiple
+        for item in ec_file_sum:
+            toks = item.partition("\t")[0]
+            ec = toks.split(";")
+            # print(ec)
+            first_item = ec[0][:5]
+            if len(ec) == 1:
+                new_ec_dict["ec"].append(ec[0])
+            else:
+                n = 0
+                for item in ec[1:]:
+                    if item[:5] == first_item:
+                        new_ec_dict["ec"].append(item[:5] + ".-")
+                        # print('same',item,first_item)
+                    elif item[:3] == first_item[:3]:
+                        new_ec_dict["ec"].append(item[:3] + ".-.-")
+                        # print('same to high level',item,first_item)
+                    elif item[:1] == first_item[:1]:
+                        new_ec_dict["ec"].append(item[:1] + ".-.-.-")
+    return new_ec_dict
+
+def build_ec_output(new_ec_dict,ec_file_from_summaries,parsed_dict,output):
+    '''
+    Splits the ec number counts and hierarchy by sample
+    '''
     ec_replace = pd.DataFrame.from_dict(new_ec_dict)
     ec_table = pd.read_table(ec_file_from_summaries)
-    print(ec_table.head(5))
     col_names = list(ec_table)[1:]
     ec_table["ec"] = ec_replace["ec"]
-    ec_hier_dict = pd.DataFrame.from_dict(d, orient="index").reset_index()
+    ec_hier_dict = pd.DataFrame.from_dict(parsed_dict, orient="index").reset_index()
     ec_hier_dict = ec_hier_dict.rename(columns={"index": "ec"})
-    print(ec_hier_dict.head(5))
-    # ec_hier_dict.head()
+    #print(ec_hier_dict.head(5))
     grouped_ec_tbl = ec_table.merge(ec_hier_dict, on="ec", how="inner")
-    print(grouped_ec_tbl.head(5))
+    #print(grouped_ec_tbl.head(5))
     grouped_ec_tbl = grouped_ec_tbl.rename(
         columns={0: "level_1", 1: "level_2", 2: "level_3", 3: "level_4"}
     )
@@ -141,16 +134,13 @@ def main(tax_file, output, ec_file, ec_file_from_summaries, dat_file):
     if len(sys.argv) == 2:
         build_tax(tax_file, output)
     else:
-        build_ec(output, ec_file, ec_file_from_summaries, dat_file)
+        new_ec_dict = parse_ec_file(ec_file_from_summaries)
+        parsed_dict = build_ec_dict(ec_file,dat_file)
+        build_ec_output(new_ec_dict, ec_file_from_summaries, parsed_dict,output)
 
 
 if __name__ == "__main__":
     p = argparse.ArgumentParser()
-    # p.add_argument("ec_converter", help="kaiju addTaxonNames output")
-    # p.add_argument("ec_dat_file", help="BLAST outfmt=6")
-    # p.add_argument(
-    #     "ec_functional_file", help="convert blastx references to KO (hsa:9373<TAB>ko:K14018)"
-    # )
     p.add_argument(
         "--tax-file",
         type=str,
