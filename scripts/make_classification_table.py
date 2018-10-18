d45d2a617cf0532f96c76d3f12d8e8e3844e0299
Joe Brown <brwnjm@gmail.com>
2018-10-18 13:37:02 -0700

    updates args

--- scripts/make_classification_table.py
+++ scripts/make_classification_table.py
@@ -11,19 +11,6 @@ from itertools import groupby
 gzopen = lambda f: gzip.open(f, "rt") if f.endswith(".gz") else open(f)
 
 
-
-def parse_kaiju_output(kaiju):
-    kaiju_classifications = dict()
-    with gzopen(kaiju) as fh:
-        for line in fh:
-            toks = line.strip("\r\n").split("\t")
-            if toks[0] == "U":
-                kaiju_classifications[toks[1]] = ["", ""]
-            else:
-                kaiju_classifications[toks[1]] = [toks[3], toks[7]]
-    return kaiju_classifications
-
-
 def parse_hmm_hits(filename):
     # contains list of split ACC [4], full seq score [1], and e-value [1]
     annotations = dict()
@@ -36,9 +23,7 @@ def parse_hmm_hits(filename):
     return annotations
 
 
-def main(
-    kaiju, hamap, dbcan, output, lca_threshold
-):
+def main(kaiju, hamap, dbcan, tigrfams, output):
     # row[4].split("~~~") -> ec, gene, product.replace("^", " "), HMM ID
     hamap_hits = parse_hmm_hits(hamap)
     # row[4].split("~~~") -> ec, enzyme class, enzyme class subfamily, HMM ID
@@ -47,9 +32,8 @@ def main(
     # row[4].split("~~~") -> ec, gene, product.replace("^", " "), HMM ID
     # ECs may be a comma delimited list
     tigrfams_hits = parse_hmm_hits(tigrfams)
-    tax_classifications = parse_kaiju_output(kaiju)
 
-    with gzopen() as ifh, open(output, "w") as ofh:
+    with gzopen(kaiju) as ifh, open(output, "w") as ofh:
         print(
             "seq_id",
             "kaiju_length",
@@ -71,36 +55,18 @@ def main(
 if __name__ == "__main__":
     p = argparse.ArgumentParser()
     p.add_argument("kaiju", help="kaiju addTaxonNames output")
-    p.add_argument("blastx", help="BLAST outfmt=6")
-    p.add_argument(
-        "gene2ko", help="convert blastx references to KO (hsa:9373<TAB>ko:K14018)"
-    )
-    p.add_argument("code2id", help="http://rest.kegg.jp/list/genome")
-    p.add_argument(
-        "kolist",
-        help="convert KO to definition (ko:K00204<TAB>fwdH; 4Fe-4S ferredoxin)",
-    )
-    p.add_argument("namesdmp")
-    p.add_argument("nodesdmp")
+    p.add_argument("hamap")
+    p.add_argument("dbcan")
+    p.add_argument("tigrfams")
     p.add_argument("output", help="classification table")
-    p.add_argument(
-        "--lca-threshold",
-        type=float,
-        default=1,
-        help="allow the lca to be determined at a node with this fraction of the hits",
-    )
     args = p.parse_args()
     logging.basicConfig(
         level=logging.INFO, datefmt="%Y-%m-%d %H:%M", format="[%(asctime)s] %(message)s"
     )
     main(
         args.kaiju,
-        args.blastx,
-        args.gene2ko,
-        args.code2id,
-        args.kolist,
-        args.namesdmp,
-        args.nodesdmp,
+        args.hamap,
+        args.dbcan,
+        args.tigrfams,
         args.output,
-        args.lca_threshold,
     )
